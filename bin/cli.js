7aa41fd1dc7774a8d78233b63a3f6ad365aa4cd8
David Berri <dwbwill@gmail.com>
2020-07-10 14:59:00 -0300

    Implements search for exchange rate on a specific date

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
 const { argv } = require('yargs')
-// .usage('Usage: $0 <command> [options]')
-// .command('create', 'Creates a PDF invoice')
-// .describe('d', 'Description')
-// .describe('q', 'Hours')
-// .describe('r', 'Rate')
-// .help('h')
-// .alias('h', 'help')
+  .usage('Usage: $0 [options]')
+  .describe('date', 'returns the exchange rate on a given <date> (dd/mm/yyyy)')
+  .alias('d', 'date')
+  .describe('raw', 'returns raw data')
+  .alias('r', 'raw')
+  // .describe('period', 'accepts two dates and returns rates in between')
+  // .alias('p', 'period')
+  // .nargs('period', 2)
+  .help('h')
+  .alias('h', 'help')
 
 require('../lib/index')(argv)
